ce7f725de20352a14a0b36c25d693f91d3a854d8
Rahul <ghostbugs00@gmail.com>
2021-10-29 13:14:10 +0530

    first commit

---
+++ topics/java.js
@@ -0,0 +1,31 @@
+console.log("java");
+
+
+// Global Variable
+let name0 = "Rahul";
+let name1 = "Inder";
+let name2 = "Seenu";
+let name3 = "Rayan";
+let name4 = "Lucifer";
+
+
+// console.log(name0 + " is a good boy\n");
+// console.log(name1 + " is a good boy\n");
+// console.log(name2 + " is a good boy\n");
+// console.log(name3 + " is a good boy\n");
+// console.log(name4 + " is a good boy\n");
+
+// Wrapping Functions
+function greet(greeTxt = " default Good Morning ", name) {
+    // local variable
+    let name0 = "Name Infinite"
+    console.log(greeTxt + name + " is a good boy\n");
+};
+let greetxt1 = "Hello, "
+
+// function call
+greet(greetxt1, name0);
+greet(greetxt1, name1);
+greet(greetxt1, name2);
+greet(name3);
+greet(greetxt1, name4);
